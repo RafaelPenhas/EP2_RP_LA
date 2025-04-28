8dd2158c8a8b1e052d92fad59d611af81e5c5f81
Rafael Penhas <rafaelkpenhas@gmail.com>
2025-04-28 19:35:24 -0300

    02

--- funcoes.py
+++ funcoes.py
@@ -8,3 +8,14 @@ def rolar_dados(numero):
        lista.append(random.randint(1,6))
   
    return lista
+
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar)
+    
+    dado = dados_rolados[dado_para_guardar]
+    dados_no_estoque.append(dado)
+    del dados_rolados[dado_para_guardar]
+
+    saida = [dados_rolados, dados_no_estoque]
+
+    return saida
+
